9d4f91c93291a855ef8994fba04257fc87d953e9
Divine <dpkreativ@gmail.com>
2022-04-02 13:46:45 +0100

    implement useSelector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function App() {
+  const value = useSelector((state) => state.value);
   return (
     <div className="flex flex-col space-y-5 items-center py-10">
-      <h1 className="text-7xl font-bold">0</h1>
+      <h1 className="text-7xl font-bold">{value}</h1>
       <button className="p-2 bg-blue-700 hover:bg-blue-800 hover:shadow font-semibold text-white text-xl rounded-lg">
         Click me
       </button>
